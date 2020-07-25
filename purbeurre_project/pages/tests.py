ac62c6b93379272313f7ec2ef871a9d263e321a2
lemarak <sdauguet@gmail.com>
2020-07-25 18:14:00 +0200

    first tests

--- purbeurre_project/pages/tests.py
+++ purbeurre_project/pages/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomepageTests(SimpleTestCase):
+
+    def test_homepage_status_code(self):
+        """ test root page """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        " test home page """
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_contains_correct_html(self): # new
+        response = self.client.get('/')
+        self.assertContains(response, 'Du gras, oui, mais de qualité !')
+
--- purbeurre_project/users/tests.py
+++ purbeurre_project/users/tests.py
@@ -1,3 +1,76 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from .views import SignupPageView, UpdateUserPageView
+
+
+class CustomUserTests(TestCase):
+    """ tests users application """
+
+    def setUp(self):
+        self.User = get_user_model()
+
+    def test_create_user(self):
+        """ Tests the creation of a user """
+
+        user = self.User.objects.create(
+            username='test',
+            email='test@example.com',
+            password='123test',
+            name='TestName',  # Field created for the application
+            bio='Ma biographie'  # Field created for the application
+        )
+        self.assertEqual(user.username, 'test')
+        self.assertEqual(user.email, 'test@example.com')
+        self.assertEqual(user.name, 'TestName')
+        self.assertEqual(user.bio, 'Ma biographie')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        """ Tests the creation of a super user """
+
+        superUser = self.User.objects.create_superuser(
+            username='admin',
+            email='admin@example.com',
+            password='123test',
+        )
+        self.assertEqual(superUser.username, 'admin')
+        self.assertEqual(superUser.email, 'admin@example.com')
+        self.assertTrue(superUser.is_active)
+        self.assertTrue(superUser.is_superuser)
+
+    # def test_duplicate_users(self):
+    #     with self.assertRaises(TypeError) as foo:
+    #         user1 = self.User.objects.create(
+    #             username='test',
+    #             email='test@example.com',
+    #             password='123test',
+    #         )
+    #         user2 = self.User.objects.create(
+    #             username='test',
+    #             email='test@example.com',
+    #             password='123test',
+    #         )
+    #     self.assertEqual("???", str(foo.exception), "???")
+
+
+class SignupPageTests(TestCase):
+
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_page(self):
+        """ test the signup page """
+        self.assertContains(self.response, 'Inscription')
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_signup_view(self):
+        """ test route """
+        view = resolve('/accounts/signup/')
+        self.assertEqual(
+            view.func.__name__,
+            SignupPageView.as_view().__name__
+        )
